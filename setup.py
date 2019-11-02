69247f508ad8831b3a78d36eab244154134b121e
Ivanq <imachug@yandex.ru>
2019-11-02 21:29:16 +0300

    Add readme to PyPI

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name="bencode_open",
     version="1.0",
     description="bencode parser and converter",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author="Ivanq",
     author_email="imachug@gmail.com",
     url="https://github.com/imachug/bencode-open",
